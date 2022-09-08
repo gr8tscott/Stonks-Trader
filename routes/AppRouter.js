037cdeedb206ab421564b50fb9706c2bc625c4fd
MatthewScott <mattwillscott95@gmail.com>
2022-09-08 12:51:08 -0400

    added user routes

---
+++ routes/AppRouter.js
@@ -0,0 +1,9 @@
+const Router = require('express').Router()
+const UserRouter = require('./UserRouter')
+const WatchlistRouter = require('./WatchlistRouter')
+const StockRouter = require('./StockRouter')
+Router.use('/user', UserRouter)
+Router.use('/watchlist', WatchlistRouter)
+Router.use('/stock', StockRouter)
+
+module.exports = Router
--- routes/Userrouter.js
+++ routes/Userrouter.js
@@ -2,5 +2,8 @@ const Router = require('express').Router()
 const controller = require('../controllers/userController')
 
 Router.get('/user', controller.GetUser)
+Router.post('/adduser', controller.createUser)
+Router.delete('/:user_d', controller.deleteUser)
+Router.put('/:user_id', controller.updateUser)
 
 module.exports = Router
