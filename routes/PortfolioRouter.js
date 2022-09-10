923f467bcf911d270d245c9a4425cc002d684135
MatthewScott <mattwillscott95@gmail.com>
2022-09-10 18:06:21 -0400

    added portfolio router

---
+++ routes/PortfolioRouter.js
@@ -0,0 +1,9 @@
+const Router = require('express').Router()
+const controller = require('../controllers/PortfolioController.js')
+
+Router.get('/portfolio', controller.getPortfolio)
+Router.post('/addportfolio', controller.createPortfolio)
+Router.delete('/:portfolio_id', controller.deletePortfolio)
+Router.put('/:portfolio_id', controller.updatePortfolio)
+
+module.exports = Router
